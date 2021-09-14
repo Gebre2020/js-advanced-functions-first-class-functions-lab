c88fdac2dbf900d3b6fece76222fe0d1d9a27130
Gebremeskel <gebre20122@gmail.com>
2021-09-14 15:38:31 -0500

    commit tree 7bc6a4c0cf072aa1e4ba7c0525806ed754a6c617

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue){
+    return function(value){
+        return multiplyValue * value;
+    };
+};
+
+function fareDoubler(num){
+  return num * 2
+}
+
+function fareTripler(num){
+    return num * 3
+  }
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+};
